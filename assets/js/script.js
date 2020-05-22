d7a196ee2908d8c30ce211585ccf4f45d7655ed3
LichKing112 <yehezkieldio35@gmail.com>
2020-05-22 19:03:11 +0800

    rewrite

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,6 @@
 ************
 -->
 */
-
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 (function() {
@@ -27,6 +26,9 @@
         menu.classList.toggle('is-active');
     });
 })();
+
+
+ 
 
 /*
 <!--
